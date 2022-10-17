38b3ed5ab08bed15b83befd85a4578d8a3b40fcc
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-10-18 01:45:20 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Bitstream Vera Sans Mono:regular:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Jetbrains Mono:regular:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
