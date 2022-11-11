a588125aeb56fd64fbe033a550e39d253f727948
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-11-12 02:26:30 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -98,37 +98,34 @@ float alpha = 0.86;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-        "#0a0f14",
-        "#c33027",
-        "#26a98b",
-        "#edb54b",
-        "#195465",
-        "#4e5165",
-        "#33859d",
-        "#98d1ce",
-        "#10151b",
-        "#d26939",
-        "#081f2d",
-        "#245361",
-        "#093748",
-        "#888ba5",
-        "#599caa",
-        "#d3ebe9",
-        [255] = 0,
-        /* more colors can be added after 255 to use with DefaultXX */
-        "#cccccc",
-        "#555555",
-        "#e6e6e6", /* fg */
-        "#121218", /* bg */
+        /* 8 normal colors */
+       [0] = "#121218", /* hard contrast: #1d2021 / soft contrast: #32302f */
+       [1] = "#cc241d", /* red     */
+       [2] = "#98971a", /* green   */
+       [3] = "#d79921", /* yellow  */
+       [4] = "#458588", /* blue    */
+       [5] = "#b16286", /* magenta */
+       [6] = "#689d6a", /* cyan    */
+       [7] = "#a89984", /* white   */
+
+       /* 8 bright colors */
+       [8]  = "#928374", /* black   */
+       [9]  = "#fb4934", /* red     */
+       [10] = "#b8bb26", /* green   */
+       [11] = "#fabd2f", /* yellow  */
+       [12] = "#83a598", /* blue    */
+       [13] = "#d3869b", /* magenta */
+       [14] = "#8ec07c", /* cyan    */
+       [15] = "#ebdbb2", /* white   */
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
