e32221c767404b99bdc627d63e51bd9995b188d9
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-11-15 20:07:39 +0800

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono:regular:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Liberation Mono:regular:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
         "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true",
