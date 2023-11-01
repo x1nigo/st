f2ba66447e359e3d6528b5a50d6095400bf6a2a3
x1nigo <christian.brodeth@gmail.com>
2023-11-02 02:39:12 +0800

    fixed opacity

--- config.h
+++ config.h
@@ -99,7 +99,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = .85;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
