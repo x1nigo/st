9be143eb54412e6bf88349284d5c8084e8767c5b
Christian Brodeth <christian.brodeth@gmail.com>
2023-12-02 14:35:58 +0800

    changed color

--- config.h
+++ config.h
@@ -148,7 +148,7 @@ static unsigned int defaultrcs = 258;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
