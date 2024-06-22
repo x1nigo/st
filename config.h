9fd0088178165039614bde56e0e78ae44b1ad261
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-06-23 00:20:43 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:bold:size=10:antialias=true:autohint=true";
+static char *font = "monospace:bold:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"NotoColorEmoji:size=10:antialias=true:autohint=true"
