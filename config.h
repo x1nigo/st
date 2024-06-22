eecd96cfec7881ecec4033d9fc4d286af50014fa
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-06-23 01:43:08 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -108,30 +108,30 @@ float alpha_def;
 static const char *colorname[] = {
         /* 8 normal colors */
 	"#282828",
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
+	"#800000",
+	"#008000",
+	"#808000",
+	"#000080",
+	"#800080",
+	"#008080",
+	"#808080",
 
         /* 8 bright colors */
-	"#1d2021",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#282828",
+	"#800000",
+	"#008000",
+	"#808000",
+	"#000080",
+	"#800080",
+	"#008080",
+	"#808080",
 
         [255] = 0,
 
         /* more colors can be added after 255 to use with DefaultXX */
-	"#1d2021", /* default bg */
-	"#d7d7d7", /* default fg */
-	"#000088", /* default cs */
+	"#080808", /* default bg */
+	"#808080", /* default fg */
+	"#808080", /* default cs */
 };
 
 
