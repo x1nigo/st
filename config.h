661b7012fa458815635a902ff47ef14f8da9682a
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-07-30 01:13:43 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -146,19 +146,13 @@ unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 258;
 
 /*
- * Default style of cursor
- * 0: blinking block
- * 1: blinking block (default)
- * 2: steady block ("â–ˆ")
- * 3: blinking underline
- * 4: steady underline ("_")
- * 5: blinking bar
- * 6: steady bar ("|")
- * 7: blinking st cursor
- * 8: steady st cursor
+ * Default shape of cursor
+ * 2: Block ("█")
+ * 4: Underline ("_")
+ * 6: Bar ("|")
+ * 7: Snowman ("☃")
  */
-static unsigned int cursorstyle = 1;
-static Rune stcursor = 0x2603; /* snowman ("â˜ƒ") */
+ static unsigned int cursorshape = 2;
 
 /*
  * Default columns and rows numbers
