d74e2005fe2f8ddf85e405c2fb37e306f47727df
x1nigo <109367483+x1nigo@users.noreply.github.com>
2026-02-27 12:42:31 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:size=10";
+static char *font = "monospace:size=10";
 /* Spare fonts */
 static char *font2[] = {
 	"NotoColorEmoji:size=8"
@@ -128,9 +128,10 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#1d2021", /* default bg */
-	"#ebdbb2", /* default fg */
-	"#5787f7", /* default cs */
+	"#1d2021", /* default bg  */
+	"#ebdbb2", /* default fg  */
+	"#ebdbb2", /* default cs  */
+	"#282828", /* default rcs */
 };
 
 
@@ -141,7 +142,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
